a44092349e7424e6f1f3b8a05c6537ccb301d2e6
kb10uy <kb10uy@live.jp>
2014-11-17 22:12:40 +0900

    [Kbtter5]形だけLinearLaserOptionを作った

--- Kbtter5/OptionOperations.cs
+++ Kbtter5/OptionOperations.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoreTweet;
+using DxLibDLL;
 
 namespace Kbtter5
 {
@@ -10,7 +12,26 @@ namespace Kbtter5
 
     public static class OptionOperations
     {
+        public static IReadOnlyList<OptionSelectionInformation> SelectionInformation = new List<OptionSelectionInformation>()
+        {
+            LinearLaserOptionInformation
+        };
 
+        #region LinearLaserOption
+        public static OptionSelectionInformation LinearLaserOptionInformation = new OptionSelectionInformation()
+        {
+            Name = "直線レーザー",
+            Description = "普通のレーザーです",
+            Operation = LinearLaserOption,
+            ModeStrings = new[] { "Kbtter5", "魔理沙" },
+            UserValueCombination = OptionSelectionValue.Direction | OptionSelectionValue.Mode
+        };
+
+        public static IEnumerator<bool> LinearLaserOption(PlayerOption option, OptionInitializationInformation info)
+        {
+            while (true) yield return true;
+        }
+        #endregion
     }
 
     public class OptionInitializationInformation
@@ -22,6 +43,43 @@ namespace Kbtter5
         public double UserDoubleValue1 { get; set; }
         public double UserDoubleValue2 { get; set; }
         public string UserStringValue { get; set; }
+    }
+
+
+    public class OptionInformation
+    {
+        public User SourceUser { get; set; }
+        public UserInformation UserInformation { get; set; }
+        public OptionOperation TargetOperation { get; set; }
+        public OptionInitializationInformation InitializationInformation { get; set; }
+
+        public OptionInformation(User u)
+        {
+            SourceUser = u;
+            UserInformation = new UserInformation(u);
+        }
+    }
+
+    public class OptionSelectionInformation
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IReadOnlyList<string> ModeStrings { get; set; }
+        public OptionOperation Operation { get; set; }
+        public OptionSelectionValue UserValueCombination { get; set; }
+
+    }
+
+    [Flags]
+    public enum OptionSelectionValue
+    {
+        Direction = 1,
+        Mode = 2,
+        Int32Value1 = 4,
+        Int32Value2 = 8,
+        DoubleValue1 = 16,
+        DoubleValue2 = 32,
+        StringValue = 64,
     }
 
     public enum OptionDirection
